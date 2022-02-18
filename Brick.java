a715b7db71fd6346ea003787fc6ccdda4707e52b
CasualQuasar <93143332+CasualQuasar@users.noreply.github.com>
2022-02-18 10:28:19 -0500

    Made brick bounce off walls

--- Brick.java
+++ Brick.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import javax.swing.GroupLayout;
 
 public class Brick {
     //Variables for the class's objects (Instance Variables)
     private int x, y ,width, height;
+    private int dx, dy;
+    private int XMin, XMax, YMin, YMax;
     private Color color;
     
     public Brick(int x, int y, int width, int height)
@@ -22,6 +25,17 @@ public class Brick {
         this.width = width;
         this.height = height;
         this.color = color;
+    }
+
+    public Brick(int x, int y, int width, int height, Color color, int dx, int dy) //Another constructor with optional values
+    {
+        this.x = x;
+        this.y = x;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.dx = dx;
+        this.dy = dy;
     }
 
     //Standard getter & setter methods
@@ -74,6 +88,66 @@ public class Brick {
         this.color = color;
     }
 
+    public int getDx()
+    {
+        return this.dx;
+    }
+    
+    public void setDx(int dx)
+    {
+        this.dx = dx;
+    }
+
+    public int getDy()
+    {
+        return this.dy;
+    }
+
+    public void setDy(int dy)
+    {
+        this.dy = dy;
+    }
+    
+    public void setXMax(int XMax)
+    {
+        this.XMax = XMax;
+    }
+
+    public int getXMax()
+    {
+        return this.XMax;
+    }
+    
+    public void setXMin(int XMin)
+    {
+        this.XMin = XMin;
+    }
+
+    public int getXMin()
+    {
+        return this.XMin;
+    }
+
+    public void setYMax(int YMax)
+    {
+        this.YMax = YMax;
+    }
+
+    public int getYMax()
+    {
+        return this.YMax;
+    }
+    
+    public void setYMin(int YMin)
+    {
+        this.YMin = YMin;
+    }
+
+    public int getYMin()
+    {
+        return this.YMin;
+    }
+
 
     public String toString()
     {
@@ -85,5 +159,26 @@ public class Brick {
     {
         g2.setColor(this.color);
         g2.fillRect(this.x, this.y, this.width, this.height);
+        g2.setStroke(new BasicStroke(3));
+        g2.setColor(color.BLACK);
+        g2.drawRect(x, y, width, height);
+    }
+
+    public void randomColor()
+    {
+        int r = (int) (Math.random()*256);
+        int b = (int) (Math.random()*256);
+        int g = (int) (Math.random()*256);
+        this.setColor(new Color(r, g, b));
+    }
+
+    public void update()
+    {
+        x += dx;
+        y += dy;
+        if (x > XMax - width || x < 0)
+        dx = -dx;
+        if (y > YMax - height || y < 0)
+        dy = -dy;
     }
 }
--- BrickGame.java
+++ BrickGame.java
@@ -4,11 +4,15 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 public class BrickGame extends JPanel implements KeyListener
 {
@@ -17,13 +21,31 @@ public class BrickGame extends JPanel implements KeyListener
    private static final int PREF_H = 400;
    private RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
    private Font font = new Font("Quicksand", Font.PLAIN, 25);
-   private Brick brick1 = new Brick(100, 50, 80, 25, Color.pink);
+   private Brick brick1, brick2;
+   private Timer timer;
 
    public BrickGame()
    {
       addKeyListener(this);
       setFocusable(true);
       requestFocus();
+      brick1 = new Brick(100, 50, 80, 25, Color.RED);
+      brick2 = new Brick(125, 10, 25, 25, Color.BLUE);
+      brick2.setDx(2);
+      brick2.setDy(2);
+      brick2.setXMax(PREF_W);
+      brick2.setYMax(PREF_H);
+
+      timer = new Timer(10, 
+      new ActionListener(){
+
+         @Override
+         public void actionPerformed(ActionEvent e) { 
+            brick2.update(); 
+            repaint();
+         }         
+      });
+      timer.start();
    }
 
    public Dimension getPreferredSize() {
@@ -36,12 +58,12 @@ public class BrickGame extends JPanel implements KeyListener
       Graphics2D g2 = (Graphics2D) g;
       g2.setRenderingHints(hints);
 
-      g2.setFont(font);
-      g2.setColor(Color.RED);
-      g2.drawString("Hello", 200, 200);
+      // g2.setFont(font);
+      // g2.setColor(Color.RED);
+      // g2.drawString("Hello", 200, 200);
 
       brick1.draw(g2);
-
+      brick2.draw(g2);
    }
 
    @Override
@@ -53,23 +75,17 @@ public class BrickGame extends JPanel implements KeyListener
 
       if (key == KeyEvent.VK_C)
       {
-         int r = (int) (Math.random()*256);
-         int b = (int) (Math.random()*256);
-         int g = (int) (Math.random()*256);
-         brick1.setColor(new Color(r, g, b));
+         brick1.randomColor();
       }
 
       if (key == KeyEvent.VK_RIGHT)
          brick1.setX(brick1.getX() + 10);
       if (key == KeyEvent.VK_LEFT)
          brick1.setX(brick1.getX() - 10);
-
       if (key == KeyEvent.VK_UP)
          brick1.setY(brick1.getY() - 10);
       if (key == KeyEvent.VK_DOWN)
          brick1.setY(brick1.getY() + 10);
-
-      repaint();
    }
 
    @Override
