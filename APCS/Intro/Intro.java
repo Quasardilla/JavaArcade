1cf722574bd974a7f090fc1ee9856f7b708c5bce
Quasar <trottinger25@pascack.org>
2022-09-07 08:32:46 -0400

    started testing JOGL

--- APCS/Intro/Intro.java
+++ APCS/Intro/Intro.java
@@ -4,21 +4,28 @@ import java.util.Scanner;
 
 public class Intro {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
 
-        System.out.println("AB\nCD\n\"E\"");
+        // System.out.println("AB\nCD\n\"E\"");
 
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        System.out.print("Please Enter Your Name: ");
-        String name = sc.nextLine();
-        System.out.println(name);
+        // System.out.print("Please Enter Your Name: ");
+        // String name = sc.nextLine();
+        // System.out.println(name);
 
-        sc.close();
+        // sc.close();
 
-        int x1 = 5;
-        int x2 = 8;
+        // double x1 = 5.0;
+        // double x2 = 8.0;
 
-        System.out.println((x1 + x2) / 2);
+        // System.out.println((x1 + x2) / 2);
+
+        //Type git --version, which will prompt an install
+        //Change directory to target directory
+        //Paste these 2 lines in console and run
+        //git clone --recurse-submodules git://jogamp.org/srv/scm/gluegen.git gluegen
+        //git clone --recurse-submodules git://jogamp.org/srv/scm/jogl.git jogl
+
     }
 }
