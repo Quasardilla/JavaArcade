6a7e6000383c514935848cb6091c864d44c64751
Quasar <trottinger25@pascack.org>
2022-09-23 09:15:58 -0400

    ending it all
    this is not a cry for help

--- APCS/Intro/Survivorio/Entity.java
+++ APCS/Intro/Survivorio/Entity.java
@@ -1,10 +1,38 @@
 package Intro.Survivorio;
 
-abstract class Entity {
+import UI.UIElement;
+
+abstract class Entity extends UIElement{
     
     protected int health;
     protected int damage;
-    protected int resistance;
+    // protected int resistance;
     protected String name;
+
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }
