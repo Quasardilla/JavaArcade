4aade36d04db1ed8b4302cd02e4c3624034108db
Quasar <trottinger25@pascack.org>
2022-12-06 13:48:42 -0500

    2d arrays

--- APCS/Exploring2DArrays/Exploring2DArrays.java
+++ APCS/Exploring2DArrays/Exploring2DArrays.java
@@ -1,5 +1,7 @@
 package Exploring2DArrays;
 
+import java.util.Arrays;
+
 public class Exploring2DArrays {
     public static void main(String[] args) {
         int[][] nums = new int[3][5];
@@ -8,6 +10,9 @@ public class Exploring2DArrays {
         print2DArrayForEach(nums);
 
         System.out.println(getColumnAverage(nums, 2));
+        System.out.println(Arrays.deepToString(new int[][] {{1, 2, 3}, {4, 5, 6}}));
+        System.out.println(Arrays.deepToString(getTransposedMatrix(new int[][] {{1, 2, 3}, {4, 5, 6}})));
+        
     }
 
     public static void print2DArray(int[][] arr2D)
@@ -57,6 +62,78 @@ public class Exploring2DArrays {
             sum += arr2d[i][col];
         return sum /= arr2d[0].length;
 
+
+    }
+    
+    public static double getRowAverage(int[][] arr2d, int row)
+    {
+        double sum = 0;
+        for(int i = 0; i < arr2d.length; i++)
+            sum += arr2d[row][i];
+        return sum /= arr2d[0].length;
+
+    }
+
+    public static double getArrayAverage(int[][] arr2d)
+    {
+        int count = 0;
+        double sum = 0;
+        for(int[] row : arr2d)
+        {
+            for(int num : row)
+            {
+                sum += num;
+                count++;
+            }
+        }
+        return sum / count;
+    }
+
+    public static double getArrayMax(int[][] arr2d)
+    {
+        int max = arr2d[0][0];
+        for(int[] row : arr2d)
+        {
+            for(int num : row)
+            {
+                max = max < num ? num : max;
+            }
+        }
+        return max;
+    }
+
+    public static double getArrayMin(int[][] arr2d)
+    {
+        int min = arr2d[0][0];
+        for(int[] row : arr2d)
+        {
+            for(int num : row)
+            {
+                min = min > num ? num : min;
+            }
+        }
+        return min;
+    }
+
+    public static void zerosOnDiagnol(int[][] arr2d)
+    {
+        for(int i = 0; i < arr2d.length; i++)
+            arr2d[i][i] = 0;
+    }
+
+    public static int[][] getTransposedMatrix(int[][] arr2d)
+    {
+        int[][] newArr2d = new int[arr2d[0].length][arr2d.length];
+        for (int i = 0; i < arr2d.length; i++)
+        {
+            for (int j = 0; j < arr2d[i].length; j++)
+            {
+                newArr2d[j][i] = arr2d[i][j];
+            }
+            
+        }
+
+        return newArr2d;
     }
 }
 
