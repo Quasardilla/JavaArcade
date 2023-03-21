d83b16b16cac5df8dfc13863c94c059eb250cce3
Quasar <trottinger25@pascack.org>
2023-03-21 10:30:47 -0400

    Insertion Sort finish & String Sort

--- APCS/Sorting/Sorter.java
+++ APCS/Sorting/Sorter.java
@@ -2,6 +2,8 @@ package Sorting;
 
 import java.util.Arrays;
 
+import Searching.FileUtility;
+
 public class Sorter {
 
 
@@ -22,17 +24,33 @@ public class Sorter {
 
     public static void insertionSort(int[] a) {
         for(int i = 0; i < a.length; i++) {
-            for(int j = 0; j < i; j++) {
-                
+            for(int j = i; j > 0 && a[j-1] > a[j]; j--) {
+                int temp = a[j-1];
+                a[j-1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    public static void insertionSort(String[] a) {
+        for(int i = 0; i < a.length; i++) {
+            for(int j = i; j > 0 && a[j-1].toLowerCase().compareTo(a[j].toLowerCase()) > 0; j--) {
+                String temp = a[j-1];
+                a[j-1] = a[j];
+                a[j] = temp;
             }
         }
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 90, 34, 7, 6, 3, 2, 2, 8, 87};
+        String[] starr;
+        try { starr = FileUtility.readFile("wordlist.txt");  } catch (Exception e) { starr = new String[]{"uh oh"};}
         // selectionSort(arr);
         insertionSort(arr);
-
+        insertionSort(starr);
+        
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(starr));
     }
 }
