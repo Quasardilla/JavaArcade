41fb123abb49bcb0bd3c4213493ead0e2177e908
MrV1ctor <vurumov25@pascack.org>
2023-03-23 13:43:27 -0400

    cleaned stuff up

--- APCS/Sorting/Sorter.java
+++ APCS/Sorting/Sorter.java
@@ -107,50 +107,47 @@ public class Sorter {
 
         // System.out.println("sorted: "+Arrays.toString(arr));
 
-        // int size = 100_000;
-        // int[] big_arr = new int[size];
-        // for (int i = 0; i < size; i++)
-        // {
-        //     big_arr[i] = (int) (Math.random()*size);
-        //     // System.out.println(big_arr[i]);
-        // }
+        int size = 100_000;
+        int[] big_arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            big_arr[i] = (int) (Math.random()*size);
+            // System.out.println(big_arr[i]);
+        }
 
-        // long nano = System.nanoTime();
+        long nano = System.nanoTime();
         // betterselectionSort(big_arr);
         // selectionSort(big_arr);
+
+        big_arr = mergeSort(big_arr);
 
         // for (int i: big_arr)
         //     System.out.println(i);
     
-        // System.out.println((System.nanoTime()-nano)/1_000_000_000.0);
- 
+        System.out.println((System.nanoTime()-nano)/1_000_000_000.0);
+        // System.out.println(Arrays.toString(big_arr));
 
-        int[] first = {};
-        int[] second= {1, 3};
-        int[] a = new int[first.length+second.length];
-        a = merge(first, second);
-        System.out.println(Arrays.toString(a));
-        int[] arr = new int[]{1, 90, 34, 7, 6, 3, 2, 2, 8, 87};
-        String[] starr;
-        try { starr = FileUtility.readFile("wordlist.txt");  } catch (Exception e) { starr = new String[]{"uh oh"};}
-        // selectionSort(arr);
-        insertionSort(arr);
-        insertionSort(starr);
+        // int[] first = {};
+        // int[] second= {1, 3};
+        // int[] a = new int[first.length+second.length];
+        // a = merge(first, second);
+        // System.out.println(Arrays.toString(a));
+        // int[] arr = new int[]{1, 90, 34, 7, 6, 3, 2, 2, 8, 87};
+        // String[] starr;
+        // try { starr = FileUtility.readFile("wordlist.txt");  } catch (Exception e) { starr = new String[]{"uh oh"};}
+        // // selectionSort(arr);
+        // insertionSort(arr);
+        // insertionSort(starr);
         
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(starr));
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(starr));
     }
 
     private static int[] mergeSort(int[] arr) 
     {
-        //base case
         if (arr.length <= 1)
-        {
             return arr;
-        }
 
-        //return statement
-        //merge the sorted left half plus the sorted right half
         int[] leftArr = new int[arr.length/2];
         int[] rightArr = new int[arr.length/2];
         
@@ -170,61 +167,13 @@ public class Sorter {
         
         for (int iFirst = 0, iSecond = 0; iFirst+iSecond<a.length;)
         {
-            //if ((iFirst < first.length && iSecond < second.length) && iFirst < first.length && first[iFirst] < second[iSecond])
-            //else if ((iFirst < first.length && iSecond < second.length) && iFirst < first.length && first[iFirst] < second[iSecond])
-            
-
-            /*
-             * do first if:
-             * second out of bounds and first in bounds
-             * second in bounds and first in bounds and first less than second
-             * 
-             * both need first in bounds
-             * second in bounds cancels out 
-             * 
-             * pseudocode
-             * if (first in bounds && (second out of bounds || first less than second))
-             * 
-             * short circuits the or so that if second is out of bounds, it doesnt check second
-             * 
-             * real code
-             * if (iFirst < first.length && (iSecond >= second.length || first[iFirst] < second[iSecond]))
-             * 
-             * do second if:
-             * first out of bounds and second in bounds
-             * second in bounds and first in bounds and second less than or equal to first
-             * 
-             * both need second in bounds
-             * first in bounds cancels out 
-             * 
-             * pseudocode
-             * if (second in bounds && (first out of bounds || second less than or equal to first))
-             * 
-             * real code
-             * 
-             * else if (iSecond < second.length && (iFirst >= first.length || second[iSecond] <= first[iFirst]))
-             * 
-             * 
-             * doesnt work T_T
-             */
-
-            // System.out.println("iFirst: "+iFirst+", iSecond: "+iSecond);
-
-            // if (iFirst >= first.length || first[iFirst] >= second[iSecond])
-            // if (iFirst < first.length || ((iFirst < first.length && iSecond < second.length) && first[iFirst] < second[iSecond]))
-            // if ((iFirst < first.length && iSecond < second.length) && iFirst < first.length && first[iFirst] < second[iSecond])            
             if (iFirst < first.length && (iSecond >= second.length || first[iFirst] < second[iSecond]))
             {
-                System.out.println("iFirst++, "+iFirst);
                 a[iFirst+iSecond] = first[iFirst];
                 iFirst++;
             }
-            // else if (iSecond >= second.length || first[iFirst] < second[iSecond])
-            // else if (iSecond < second.length || ((iFirst < first.length && iSecond < second.length) && first[iFirst] >= second[iSecond]))
-            // else if ((iFirst < first.length && iSecond < second.length) && iFirst < first.length && first[iFirst] < second[iSecond])
             else if (iSecond < second.length && (iFirst >= first.length || second[iSecond] <= first[iFirst]))
             {
-                System.out.println("iSecond++, "+iSecond);
                 a[iFirst+iSecond] = second[iSecond];
                 iSecond++;
             }
@@ -235,36 +184,4 @@ public class Sorter {
     }   
 
 
-    // private static int[] merge(int[] first, int[] second) {
-    //     int[] a = new int[first.length + second.length];
-        
-    //     // for (int iFirst = 0, iSecond = 0; iFirst+iSecond<a.length;)
-
-        //iFirst < first.length || ((iFirst < first.length && iSecond < second.length) && first[iFirst] < second[iSecond])
-        //iSecond < second.length || ((iFirst < first.length && iSecond < second.length) && !(first[iFirst] < second[iSecond]))
-    //     int iFirst = 0, iSecond = 0;
-    //     while (iFirst < first.length && iSecond < second.length) {
-    //         if (first[iFirst] < second[iSecond]) {
-    //             a[iFirst+iSecond] = first[iFirst];
-    //             iFirst++;
-    //         } else {
-    //             a[iFirst+iSecond] = second[iSecond];
-    //             iSecond++;
-    //         }
-    //     }
-        
-    //     while (iFirst < first.length) {
-    //         a[iFirst+iSecond] = first[iFirst];
-    //         iFirst++;
-    //     }
-        
-    //     while (iSecond < second.length) {
-    //         a[iFirst+iSecond] = second[iSecond];
-    //         iSecond++;
-    //     }
-        
-    //     return a;
-    // }
-    
-    
 }
